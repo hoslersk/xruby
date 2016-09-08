7319766f0371f8433f4324ed6a38b7aac3470297
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-08 15:20:25 -0400

    added empty tests

---
+++ exercises/rectangles/rectangles_test.rb
@@ -0,0 +1,9 @@
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+require_relative 'rectangles.rb'
+
+class RectangleTest < Minitest::Test
+  def test_count
+    # assert_equal 1, Stuff.new.count
+  end
+end
---
+++ exercises/rectangles/spec/rectangle_spec.rb
@@ -0,0 +1,8 @@
+describe "Rectangle basics" do
+  describe '' do
+    it '' do
+      # game = Game.new
+      # expect(game).to be_a(Game)
+    end
+  end
+end
