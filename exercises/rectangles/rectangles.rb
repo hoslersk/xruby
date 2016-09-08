e3452d3177f2f4ccbbb8164ecbe5206c41307545
aehmt <calisahmet@gmail.com>
2016-09-08 15:53:17 -0400

    tests are added

--- exercises/rectangles/rectangles.rb
+++ exercises/rectangles/rectangles.rb
@@ -1,3 +1,5 @@
-def rectangles
+class Rectangle
 
 end
+
+
--- exercises/rectangles/rectangles_test.rb
+++ exercises/rectangles/rectangles_test.rb
@@ -1,9 +1,71 @@
 gem 'minitest', '>= 5.0.0'
+gem 'minitest-colorize'
 require 'minitest/autorun'
 require_relative 'rectangles.rb'
 
 class RectangleTest < Minitest::Test
-  def test_count
-    # assert_equal 1, Stuff.new.count
+
+  def test_zero_area_1
+    rectangle = Rectangle.new
+    assert_equal 0, rectangle.count 
+  end
+
+  def test_zero_area_2
+    line = ''
+    rectangle = Rectangle.new
+    assert_equal 0, rectangle.count(line) 
+  end
+
+  def test_zero_area_3
+    line = ' '
+    rectangle = Rectangle.new
+    assert_equal 0, rectangle.count(line) 
+  end
+
+  def test_one_rectangle
+    line = ["+-+", "| |", "+-+", ]
+    rectangle = Rectangle.new
+    assert_equal 1, rectangle.count(line) 
+  end
+
+
+  def test_two_rectangle_no_shared_parts
+    line = ["  +-+", "  | |", "+-+-+", "| |  ", "+-+  " ]
+    rectangle = Rectangle.new
+    assert_equal 2, rectangle.count(line) 
+  end
+
+  def test_five_rectangles_three_region
+    line = ["  +-+", "  | |", "+-+-+", "| | |", "+-+-+" ]
+    rectangle = Rectangle.new
+    assert_equal 5, rectangle.count(line) 
+  end
+
+  def test_incomplete_rectangles
+    line = ["  +-+", "    |", "+-+-+", "| | -", "+-+-+" ]
+    rectangle = Rectangle.new
+    assert_equal 1, rectangle.count(line) 
+  end
+
+  def test_complicated
+    line = ["+------+----+", "|      |    |", "+---+--+    |", "|   |       |", "+---+-------+" ]
+    rectangle = Rectangle.new
+    assert_equal 3, rectangle.count(line) 
+  end
+
+  def test_not_so_complicated
+    line = ["+------+----+", "|      |    |", "+------+    |", "|   |       |", "+---+-------+" ]
+    rectangle = Rectangle.new
+    assert_equal 2, rectangle.count(line) 
   end
 end
+
+
+
+
+
+
+
+
+
+
